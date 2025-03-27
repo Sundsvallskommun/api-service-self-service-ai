6bfc1d92b7bf2f49b5773821e94faca0b1d6036b
Joakim Schürer <joakim.schurer@sundsvall.se>
2025-03-27 07:36:41 +0100

    UF-15220: Changed Lime integration to WSO2
    - Changed to use WSO2 for the Lime integration as this is the only way
    to reach the Lime service
    - Updated to latest Intric certificate

--- src/main/java/se/sundsvall/selfserviceai/integration/lime/configuration/LimeConfiguration.java
+++ src/main/java/se/sundsvall/selfserviceai/integration/lime/configuration/LimeConfiguration.java
@@ -1,13 +1,17 @@
 package se.sundsvall.selfserviceai.integration.lime.configuration;
 
 import static java.util.Optional.ofNullable;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import feign.RequestInterceptor;
+import java.util.List;
 import org.springframework.cloud.openfeign.FeignBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import se.sundsvall.dept44.configuration.feign.FeignConfiguration;
 import se.sundsvall.dept44.configuration.feign.FeignMultiCustomizer;
+import se.sundsvall.dept44.configuration.feign.decoder.ProblemErrorDecoder;
 
 @Import(FeignConfiguration.class)
 public class LimeConfiguration {
@@ -17,9 +21,11 @@ public class LimeConfiguration {
 	private static final String API_KEY_HEADER_NAME = "X-Client-Secret";
 
 	@Bean
-	FeignBuilderCustomizer feignBuilderCustomizer(final LimeProperties limeProperties) {
+	FeignBuilderCustomizer feignBuilderCustomizer(final LimeProperties limeProperties, final ClientRegistrationRepository clientRegistrationRepository) {
 		return FeignMultiCustomizer.create()
+			.withErrorDecoder(new ProblemErrorDecoder(CLIENT_ID, List.of(NOT_FOUND.value())))
 			.withRequestTimeoutsInSeconds(limeProperties.connectTimeoutInSeconds(), limeProperties.readTimeoutInSeconds())
+			.withRetryableOAuth2InterceptorForClientRegistration(clientRegistrationRepository.findByRegistrationId(CLIENT_ID))
 			.withRequestInterceptor(requestInterceptor(limeProperties.apiKey()))
 			.composeCustomizersToOne();
 	}
